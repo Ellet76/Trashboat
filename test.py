5b70fa2fb0ed5a9443a365cdf1c093caf62c9861
Linus <linus@torkel.se>
2025-09-16 13:24:52 +0200

    correction

--- test.py
+++ test.py
@@ -1,23 +1,27 @@
 import pygame as pg
 pg.init()
 
-window = pg.display.set_mode((640, 640))
+
+window_width = 640
+window_height = 640
+window = pg.display.set_mode((window_width, window_height))
 pg.display.set_caption("Trash boat")
 
 blue = (0, 64, 128)
-window.fill(blue)
+
+
+def draw():
+    window.fill(blue)
 
 
 run = True
 while run:
+    draw()
     pg.display.update()
-    window.fill(blue)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             run = False
-    pg.draw.rect(window, (124,124,124), (0,0, 640, 40))
-    pg.draw.rect(window, (124,124,124), ((0,560, 640, 560)))
-
+    
 
 
